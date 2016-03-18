a2e32fc156ca63f39dea098ace072b1fdf08475a
mcarter78 <sjsharksftw@gmail.com>
2016-03-17 22:23:14 -0700

    comments comments comments

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -1,59 +1,77 @@
 var User = require('../models/user');
 var Trip = require('../models/trip');
 
+// this page can be refactored to replace any occurance of req.currentUser function
+// with a check to see if req.user is true
+
 var tripsController = {
   newTrip: function(req, res){
     req.currentUser(function(err, user){
-      console.log('Current User:', user);
+      // if user is not logged in...
       if(user === null){
+        // redirect to login
         res.redirect('/login');
       } else {
-      res.render('./partials/trips/new');
+        // redirect to create trip
+        res.render('./partials/trips/new');
       }
 		});
   },
   createTrip: function(req, res){
     req.currentUser(function(err, user){
-			console.log('Current User:', user);
+      // if user is not logged in...
       if (user === null){
+        // redirect to login
         res.redirect('/login');
       } else {
+        // create a trip object
         var trip = {};
+        // set from and to locations
     		trip.fromLocation = req.body.fromLocation;
     		trip.toLocation = req.body.toLocation;
-        // set Trips id's based on whether user choses driver or rider
+        // set trip's id's based on whether user choses driver or rider
         if (req.body.userType === 'Driver') {
           trip.driverId = req.session.userId;
         }
         else if (req.body.userType === 'Rider') {
           trip.riderId = req.session.userId;
         }
-        console.log(trip);
+        // pass the completed trip object into create method
     		Trip.create(trip, function(err, trip){
     			if (err) res.status(500).send();
+          // get the logged in user's id
           var id = req.session.userId;
+          // find that user in the database
           User.findById(id, function(err, user){
-            console.log(user.trips);
+            // add the trip to that user's trips
             user.trips.push(trip);
-            console.log(user);
             user.save();
           });
+          // send the trip object back as response
     			res.status(201).send(JSON.stringify(trip));
     		});
       }
     });
   },
   showTrip: function( req, res ) {
+    // get the trip id
     var id = req.params.id;
-    console.log("it works!",id);
+    // get the logged in user's id
     var userId = req.user._id;
+    // find the trip in the database
     Trip.findById(id, function(err, trip){
+      // set the trip's driverId to that of the logged in user
       trip.driverId = userId;
       trip.save(function(err, updatedTrip){
+        // find the rider associated with the trip
         User.findById(updatedTrip.riderId, function(err, rider){
+          // set the rider's trip driverId also
           rider.trips[0].driverId = userId;
           rider.save();
+          // find the driver associated with the trip
           User.findById(updatedTrip.driverId, function(err, driver){
+            // render the show view, passing in data for the newly updated trip,
+            // the rider, and the driver
             res.render('./partials/trips/show', { trip: JSON.stringify(updatedTrip),
               rider: JSON.stringify(rider), driver: JSON.stringify(driver) });
           });
@@ -62,15 +80,20 @@ var tripsController = {
     });
   },
   tripsApi: function(req, res) {
+    // find all trips
     Trip.find({}, function(err, trips){
       if (err) res.status(500).send();
+      // send back the trips as JSON data
       res.status(200).send(JSON.stringify(trips));
     });
   },
   updateTrip: function(req, res) {
+    // get the trip's id
     var id = req.params.id;
+    // find that trip in the database
     Trip.findById(id, function(err, trip){
       if (err) return console.log(err);
+      // set that trip's location to the new form locations
       trip.fromLocation = parseInt(req.body.fromLocation);
       trip.toLocation = parseInt(req.body.toLocation);
       trip.save(function(err, updatedTrip){
@@ -79,28 +102,35 @@ var tripsController = {
     });
   },
   deleteTrip: function(req, res) {
+    // get the trip's id
     var id = req.params.id;
-    console.log(id);
+    // find and remove that trip from the database
     Trip.findByIdAndRemove({_id: id}, function(err){
         if (err) res.status(500).send();
     });
-    // res.redirect('./partials/trips/new');
-  }, 
+  },
   checkForDriver: function(req, res){
+    // get the logged in user's id
     var id = req.user._id;
-    console.log(id);
+    // find that user in the database
     User.findById(id, function(err, user){
+      // if that user's trip has no driver
       if (user.trips[0].driverId === undefined){
+        // send back null (empty string)
         res.send(null);
       } else {
+        // find the driver in the users database
         User.findById(user.trips[0].driverId, function(err, driver){
+          // THIS CODE IS BROKEN, SEEMS TO SEND THE WRONG #
+          // THIS IS THE CODE THAT CONNECTS OUR DRIVER & RIDER
+          // send back that driver's trip's id
           res.send(driver.trips[0]._id);
         });
-        
       }
     });
   },
   completeTrip: function(req, res) {
+    // render the trip completed page
     res.render('./partials/trips/here');
   }
 };
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,19 +4,23 @@ var bcrypt = require('bcrypt');
 
 var usersController = {
 	newUser: function (req, res){
+		// render the new user form page
 		res.render('./partials/users/new');
 	},
 	createUser: function(req, res){
-		console.log('creating');
+		// create a new user object
 		var user = {};
+		// pass in values from the form
 		user.name = req.body.name;
 		user.email = req.body.email;
 		user.password = req.body.password;
 		user.mobileNumber = req.body.mobileNumber;
 		user.imageUrl = req.body.imageUrl;
+		// create a secure user with hashed password
 		User.createSecure(user, function(err, userData) {
-			console.log(userData);
+			// log the new user in
 			req.login(userData);
+			// sanity check to log the user after login
 			req.currentUser(function(err, otherUser){
 				console.log("ITS ME",otherUser);
 			});
@@ -25,15 +29,18 @@ var usersController = {
 		});
 	},
 	loginUser: function(req, res){
+		// render the login form page
 		res.render('login');
 	},
 	authenticateUser: function(req, res){
-		console.log(req.body);
+		// collect the data from login form
 		var email = req.body.email;
 		var password = req.body.password;
 		// authenticate user
 		User.authenticate(email, password, function(err, user){
+			// login the user
 			req.login(user);
+			// sanity check to log the newly logged in user
 			req.currentUser(function(err, otherUser){
 				console.log("ITS ME",otherUser);
 			});
@@ -41,45 +48,52 @@ var usersController = {
 		});
 	},
 	logoutUser: function(req, res){
+		// logout the user
 		req.logout();
+		// sanity check to make sure user is logged out
 		req.currentUser(function(err, otherUser){
 			console.log("ITS ME",otherUser);
 		});
+		// redirect to root
 		res.redirect('/');
 	},
 	//render riders
 	indexUsers: function(req, res){
-      console.log("indexing");
-	    //Find all users
-	    User.find({}, function(err,users){
-      //Finds logged in user
-      var id = req.session.userId;
-      var sendUsers = [];
-				User.findById(id, function(err, user){
-					//PASTING
-					for(var i = 0; i < users.length; i++){
-						console.log(users[i]._id);
-						console.log(id);
-				    if ((users[i].trips[0] === undefined) || (user.trips[0] === undefined)) {
-				        continue;
-				    }
-				    else if (users[i]._id == id){
-				        console.log('USER MATCH', user[i]);
-				        continue;
-				    }
-				    else if (users[i].trips[0].riderId === undefined){
-					    	continue;
-				    }
-				    else if((users[i].trips[0].fromLocation === user.trips[0].fromLocation)&&
-				        (users[i].trips[0].toLocation === user.trips[0].toLocation)){
-				            // console.log("WE HAVE A MATCH:", users[i]);
-				    sendUsers.push(users[i]);
-				    }
-					}
-				//ends pasting
-		  			res.render('./partials/trips/index', { loggedInUser: JSON.stringify(user), allUsers: JSON.stringify(sendUsers)});
-		    });
-	    }); /*Find all users ends here */
+    //Find all users
+    User.find({}, function(err,users){
+    //Finds logged in user
+    var id = req.session.userId;
+		// creates empty array to collect response data
+    var sendUsers = [];
+		User.findById(id, function(err, user){
+			// iterate through each user
+			for(var i = 0; i < users.length; i++){
+				// if user has no trips...
+		    if ((users[i].trips[0] === undefined) || (user.trips[0] === undefined)) {
+					// skip that one
+		      continue;
+		    }
+				// if user is the logged in user
+		    else if (users[i]._id == id){
+	        // skip that one
+	        continue;
+		    }
+				// if that user's trip has no rider (user is a driver)
+		    else if (users[i].trips[0].riderId === undefined){
+					// skip that one
+			    continue;
+		    }
+				// if the user's from & to locations match the logged in user's...
+		    else if((users[i].trips[0].fromLocation === user.trips[0].fromLocation)&&
+		        (users[i].trips[0].toLocation === user.trips[0].toLocation)){
+		      // push that user into the response array
+		    	sendUsers.push(users[i]);
+		    }
+			}
+			// render the index page and send back data for the logged in user, and the response array with matched riders
+	  	res.render('./partials/trips/index', { loggedInUser: JSON.stringify(user), allUsers: JSON.stringify(sendUsers)});
+	    });
+    });
   }
 };
 
